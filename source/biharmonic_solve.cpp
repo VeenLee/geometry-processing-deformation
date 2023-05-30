6de0e5d6e42987497d9a2ad61ca8c3fa87e76e46
VeenLee <415049053@qq.com>
2023-05-30 21:52:15 +0800

    Update biharmonic_solve.cpp

--- source/biharmonic_solve.cpp
+++ source/biharmonic_solve.cpp
@@ -1,11 +1,26 @@
 #include "biharmonic_solve.h"
 #include <igl/min_quad_with_fixed.h>
+#include <igl/harmonic.h>
 
 void biharmonic_solve(
   const igl::min_quad_with_fixed_data<double> & data,
   const Eigen::MatrixXd & bc,
   Eigen::MatrixXd & D)
 {
-  // REPLACE WITH YOUR CODE
-  D = Eigen::MatrixXd::Zero(data.n,3);
+	//code 1
+	{
+		Eigen::MatrixXd B(data.n, 3), Beq;
+		B = Eigen::MatrixXd::Zero(data.n, 3);
+		igl::min_quad_with_fixed_solve(data, B, bc, Beq, D);
+	}
+
+
+	//code 2
+	{
+		//const int size = data.n;
+		//const int dims = bc.cols();
+
+		//min_quad_with_fixed_solve(data, Eigen::MatrixXd::Zero(size, dims),
+		//	bc, Eigen::MatrixXd::Zero(0, dims), D);
+	}
 }
