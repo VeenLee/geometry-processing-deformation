5be0966c582a59f22d43cfed15a68f38463e23a2
VeenLee <415049053@qq.com>
2023-05-30 21:52:33 +0800

    Update biharmonic_precompute.cpp

--- source/biharmonic_precompute.cpp
+++ source/biharmonic_precompute.cpp
@@ -10,6 +10,45 @@ void biharmonic_precompute(
   const Eigen::VectorXi & b,
   igl::min_quad_with_fixed_data<double> & data)
 {
-  // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+    //code 1
+    {
+        // Find L, M
+        Eigen::SparseMatrix<double> L, M;
+        igl::cotmatrix(V, F, L);
+        igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+        // Compute M_inverse
+        Eigen::SparseMatrix<double> M_inverse(M.rows(), M.rows());
+        Eigen::VectorXd M_inverse_dia;
+        M_inverse_dia = Eigen::ArrayXd::Ones(M.rows()) / M.diagonal().array();
+        for (int i = 0; i < M.rows(); i++) {
+            M_inverse.insert(i, i) = M_inverse_dia[i];
+        }
+
+        // Compute Q:
+        Eigen::SparseMatrix<double> Q, Aeq;
+        Q = L.transpose() * M_inverse * L;
+
+        // Compute data:
+        igl::min_quad_with_fixed_precompute(Q, b, Aeq, false, data);
+    }
+
+
+    //code 2
+    {
+        //data.n = V.rows();
+        //Eigen::SparseMatrix<double> L;
+        //igl::cotmatrix(V, F, L);
+
+        //Eigen::SparseMatrix<double> M;
+        //igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+
+        //Eigen::SparseMatrix<double> M_inverse;
+        //igl::invert_diag(M, M_inverse);
+
+        //Eigen::SparseMatrix<double> Q;
+        //Q = L.transpose() * M_inverse * L;
+
+        //igl::min_quad_with_fixed_precompute(Q, b, Eigen::SparseMatrix<double>(), true, data);
+    }
 }
